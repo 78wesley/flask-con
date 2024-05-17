b4c427a430f253ad656677061c80f30a20739658
ruijsit-laptop <wruijsbroek@outlook.com>
2024-05-17 16:17:20 +0200

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,26 +3,24 @@ from flask import Flask
 from flask_crontab import Crontab
 import datetime
 import os
+from logging.config import dictConfig
 
 app = Flask(__name__)
 crontab = Crontab(app)
 
-from logging.config import dictConfig
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {"wsgi": {"class": "logging.StreamHandler", "stream": "ext://flask.logging.wsgi_errors_stream", "formatter": "default"}},
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
     }
-})
+)
+
 
 def ensure_crontabs():
     # Ensure the crontabs are added; avoid duplicates by clearing and re-adding.
